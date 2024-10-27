8fd84104205daa84a84d9bc3dba4f9b6f4048fb3
huguesva <vanasselhugues@gmail.com>
2024-10-26 22:07:45 -0400

    rm metrics in supervised

--- stable_ssl/supervised.py
+++ stable_ssl/supervised.py
@@ -35,21 +35,3 @@ class Supervised(BaseModel):
             commit=False,
         )
         return F.cross_entropy(preds, self.data[1])
-
-    def initialize_metrics(self):
-
-        nc = self.config.data.num_classes
-        train_acc1 = MulticlassAccuracy(num_classes=nc, top_k=1)
-        acc1 = MulticlassAccuracy(num_classes=nc, top_k=1)
-        acc5 = MulticlassAccuracy(num_classes=nc, top_k=5)
-        acc1_by_class = MulticlassAccuracy(num_classes=nc, average="none", top_k=1)
-        acc5_by_class = MulticlassAccuracy(num_classes=nc, average="none", top_k=5)
-        self.metrics = torch.nn.ModuleDict(
-            {
-                "train/step/acc1": train_acc1,
-                "eval/epoch/acc1": acc1,
-                "eval/epoch/acc5": acc5,
-                "eval/epoch/acc1_by_class": acc1_by_class,
-                "eval/epoch/acc5_by_class": acc5_by_class,
-            }
-        )
