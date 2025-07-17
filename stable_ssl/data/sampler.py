af151e07d767d9b9eec7ac6eae93eb9c8fe2f09c
lucas-maes <lucas.maes7711@gmail.com>
2025-07-17 12:13:58 -0400

    add unit test

--- stable_ssl/data/sampler.py
+++ stable_ssl/data/sampler.py
@@ -60,7 +60,7 @@ class RepeatedRandomSampler(torch.utils.data.DistributedSampler):
                 (self._data_source_len - self.num_replicas) / self.num_replicas  # type: ignore[arg-type]
             )
         else:
-            self.num_samples = self._data_source_len
+            self.num_samples = self._data_source_len // self.num_replicas  # type: ignore[arg-type]
 
         if not isinstance(self.replacement, bool):
             raise TypeError(
@@ -75,18 +75,9 @@ class RepeatedRandomSampler(torch.utils.data.DistributedSampler):
     #     return (len(self.data_source) * self.n_views) // self.num_replicas
 
     def __iter__(self) -> Iterator[int]:
-        """Iterate over the indices of the dataset.
-
-        Note: need to append the worker_id to the seed to ensure that each worker
-        gets a different random sequence. worker_init_fn is called only at the beginning
-
-        https://pytorch-lightning.readthedocs.io/en/1.7.7/api/pytorch_lightning.utilities.seed.html#pytorch_lightning.utilities.seed.seed_everything
-        """
         n = self._data_source_len
         g = torch.Generator()
-        worker_info = torch.utils.data.get_worker_info()
-        worker_id = worker_info.id if worker_info else 0
-        g.manual_seed(self.seed + self.epoch + worker_id)
+        g.manual_seed(self.seed + self.epoch)
 
         if self.replacement:
             raise NotImplementedError()
